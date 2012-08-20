9758c82024d527a846cf30a2e79483421b264f00
Andrey Osipov <aosipov.me@gmail.com>
2012-08-20 15:24:04 +0300

    adf

---
+++ src/main/java/exam/basics/collections/map/hashtable/HashtableTest.java
@@ -0,0 +1,16 @@
+package exam.basics.collections.map.hashtable;
+
+import java.util.Hashtable;
+import java.util.Map;
+
+/**
+ * User: andrey.osipov
+ * Date: 8/9/12
+ * Time: 11:57 AM
+ */
+public class HashtableTest {
+
+    public static void main(String[] args) {
+        Map hashtable = new Hashtable();
+    }
+}
